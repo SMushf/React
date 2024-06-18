1bec2f1eca3347f8ac0f7858f273b3b70fa4e736
Shaikh Musharraf <s5m7musharraf@gmail.com>
2024-06-19 03:04:22 +0530

    Created Tailwind Project

---
+++ 05tailwindpropsproject/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Cards from './Components/Card'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Cards />
+  </React.StrictMode>,
+)
