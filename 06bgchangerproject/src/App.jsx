7c701c9b02a2b8f16c8754dfc2071bad0c3f685e
Shaikh Musharraf <s5m7musharraf@gmail.com>
2024-06-19 22:42:06 +0530

    Created Password Generator

---
+++ 06bgchangerproject/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [bgChanger, setBg] = useState("white")
+
+  return (
+    <>
+      <div id='bgColor' className='h-screen width-full grid grid-cols-0 gap-4 place-items-center h-56 flex space-x-10' style={{backgroundColor: bgChanger}}>
+        <div className='flex space-x-10'>
+          <button className='rounded-md hover:bg-white hover:text-black bg-violet-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#A78BFA')}>violet</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-indigo-700 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#4338CA')}>indigo</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-blue-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#60A5FA')}>blue</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-green-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#4ADE80')}>green</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-yellow-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#FACC15')}>yellow</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-orange-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#FB923C')}>orange</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-red-400 p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('#F87171')}>red</button>
+          <button className='rounded-md hover:bg-white hover:text-black bg-black p-2 w-40 text-white font-sans text-xl ' onClick= {() => setBg('black')}>black</button>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
