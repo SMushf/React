b7652097ca83a82d134a359cc03e834a7b655f04
Shaikh Musharraf <s5m7musharraf@gmail.com>
2024-07-04 08:01:27 +0530

    WebProjectCompleted

--- 09webprojwithreactrouter/src/components/Github/Github.jsx
+++ 09webprojwithreactrouter/src/components/Github/Github.jsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from "react"
-
+//import { useLoaderData } from "react-router-dom"
 
 export default function Github() {
 
+    //const data = useLoaderData()
     const [data, setData] = useState([])
 
     useEffect(() => {
-        fetch('https:/api.github.com/users/SMushf')
+        fetch(`https://api.github.com/users/SMushf`)
         .then((res) => res.json())
-        .then((res) => setData(res))
+        .then(data => setData(data))
+        console.log(data)
     }, [])
   return (
-    <div className="bg-gray-600 text-white text-3xl p-4 text-center">
-      Github Follower: {data}
+    <div className="bg-gray-600 text-white text-3xl p-4 text-center grid  place-content-center">
+      <p>Hey! Welcome {data.name}</p>
+      <img src={data.avatar_url} />
     </div>
   )
 }
 
+
+// export const GithubAPI = async () => {
+//   const response = await fetch('https://api.github.com/users/SMushf')
+//   return response.json()
+  
+// }
